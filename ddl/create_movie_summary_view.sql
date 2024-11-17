ee6e9a5a446003cfe5191c3ef9488c82d78012dc
Christa <82023874+christa-ux@users.noreply.github.com>
2024-11-17 15:28:41 -0500

    Update create_movie_summary_view.sql

--- ddl/create_movie_summary_view.sql
+++ ddl/create_movie_summary_view.sql
@@ -1,17 +1,19 @@
+--part6
+
 CREATE VIEW movie_summary AS
 SELECT 
-    m.tmdbID,
-    m.imdbID,
+    m."tmdbID",
+    m."imdbID",
     m.title,
     m.plot AS description,
-    m.content_Rating,
+    m."content_Rating",
     COUNT(DISTINCT mk.keywordID) AS num_keywords,
     COUNT(DISTINCT mc.countryCode) AS num_countries
 FROM 
     "Movie" m
 LEFT JOIN 
-    "Moviekeyword" mk ON m.mID = mk.movieID
+    "Moviekeyword" mk ON m."mID" = mk.movieID
 LEFT JOIN 
-    "Moviecountry" mc ON m.mID = mc.movieID
+    "Moviecountry" mc ON m."mID" = mc.movieID
 GROUP BY 
-    m.tmdbID, m.imdbID, m.title, m.plot, m.content_Rating;
+    m."tmdbID", m."imdbID", m.title, m.plot, m."content_Rating";
